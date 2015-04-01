c431e31e0506582c295f7f368e7bad8065d62aed
guming <439309415@qq.com>
2015-04-02 02:49:10 +0800

    show the name,type,value of a module itme

--- chapters9/exe9_7.py
+++ chapters9/exe9_7.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
-name = raw_input("Please input the module name: ")
-obj = __import__(name)
-ls = dir(name)
-for item in ls:
-    print 'name:',item
-    print 'type:',type(getattr(obj,item))
-    print 'value',getattr(obj,item)
-    print 
+def delmodule(name):
+    obj = __import__(name)
+    ls = dir(obj)
+    for item in ls:
+        print 'name: ', item
+        print 'type: ', type(getattr(obj,item))
+        print 'value ', getattr(obj,item)
+        print 
+
+
+if __name__ =="__main__":
+    name = raw_input("Please input the module name:")
+    delmodule(name)
+
