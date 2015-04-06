e17d09a02d5cad8636f523bbf94c03cb593b1d78
guming <439309415@qq.com>
2015-04-06 16:52:40 +0800

    max and mix function

---
+++ chapters11/exe11_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-------------------a--------
+def max2(num1,num2):
+   return num1 if num1 > num2 else num2
+
+def min2(num1,num2):
+   return num1 if num1 < num2 else num2
+
+print max2(4,8)
+print min2(4, 8)
+
+#-------------a--------------
