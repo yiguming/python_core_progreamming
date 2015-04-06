dbbfa0594a592d81fd7e7df5fa7c4bb37af00a5e
guming <439309415@qq.com>
2015-04-06 17:00:39 +0800

    my_max ,my_min

--- chapters11/exe11_2.py
+++ chapters11/exe11_2.py
@@ -9,4 +9,19 @@ def min2(num1,num2):
 print max2(4,8)
 print min2(4, 8)
 
-#-------------a--------------
+#-------------b--------------
+
+def my_max(*args):
+    return reduce(max2,args)
+
+
+
+def my_min(*args):
+    return reduce(min2,args)
+
+
+print my_max(1,2,3,4,5,6,7,8)
+print my_min(1,2,3,4,5,6,7,8)
+
+
+ 
