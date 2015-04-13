95b726c8e362b7d9db8ba6107b112635e689f5d5
guming <439309415@qq.com>
2015-04-13 20:41:45 +0800

    class time60

---
+++ chapters13/time60.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+class Time60(object):
+    'Time60 - track hours and minutes'
+
+    def __init__(self,hr,min):
+        'Time60 constructor - takes hours and minutes'
+        self.hr = hr
+        self.min = min
+
+    def __str__(self):
+        'Time60 - string representation'
+        return "%d:%d" %(self.hr,self.min)
+
+    def __add__(self,other):
+        'Time60 - overloading the addition operator'
+        return self.__class__(self.hr + other.hr,self.min+other.min)
+
+    __repr__ = __str__
+
+    def __iadd__(self,other):
+        'Time 60 - overloading in-place addtion'
+        self.hr +=other.hr
+        self.min += other.min
+        return self
+
+
+if __name__ == "__main__":
+    mon = Time60(10,30)
+    tue = Time60(11,15)
+    print mon
+    print id(mon)
+    mon += tue
+    print id(mon)
+    print mon
+
+
+    
+
+
+
