c0be3b732d7177bddffa38856fc5c12be060c494
guming <439309415@qq.com>
2015-04-16 00:28:43 +0800

    fix the bug

--- chapters13/exe13_15.py
+++ chapters13/exe13_15.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python
 class CapOpen(object):
-    def __init__ (self,fn,mode = 'r',buf =-1):
-        self.file = open(self,mode,buf)
-
+    def __init__(self, fn, mode = "r", buf = -1):
+        self.file = open(fn, mode, buf)
     def __str__(self):
         return str(self.file)
-
     def __repr__(self):
-        return 'self.file'
-
-    def write(self,line):
+        return repr(self.file)
+    def write(self, line):
         self.file.write(line.upper())
-
-    def __getattr__(self,attr):
-        return getattr(self.file,attr)
-
-
-
-
+    def __getattr__(self, attr):
+        return getattr(self.file, attr)
 if __name__ == "__main__":
-    f = CapOpen('exe13_14.py','r')
-    for eachLine in f:
-        print eachLine
+    fobj = CapOpen("data.txt")
+    print fobj.read()
+    fobj.close()    
